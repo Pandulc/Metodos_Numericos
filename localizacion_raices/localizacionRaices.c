24d8ff87c259ed7aca4d1e4fd0532429ca384ef5
Gabriel <gabiguibe@gmail.com>
2022-09-24 17:50:29 -0300

    Some changes for practice

--- localizacion_raices/localizacionRaices.c
+++ localizacion_raices/localizacionRaices.c
@@ -4,29 +4,36 @@
 // PROTOTIPADO DE FUNCIONES
 
 int menu();
+
 double funcion(double x);
+
 double funcionAux(double x);
+
 void biseccion();
+
 void regulaFalsi();
+
 void puntoFijo();
+
 void newton_raphson();
+
 void secante();
 
 // DEFINICION DE f(x)
 
-double funcion(double x){
-    return exp(-x) - x;
+double funcion(double x) {
+    return x * cosh(10 / x) - x - 6;
 }
 
-double funcionAux(double x){
-    return -exp(-x) - 1;
+double funcionAux(double x) {
+    return cosh(10 / x) - x * sinh(10 / x) * (10 / pow(x, 2));
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
     int option;
-    do{
+    do {
         option = menu();
-        switch (option){
+        switch (option) {
             case 0:
                 break;
             case 1:
@@ -52,7 +59,7 @@ int main(int argc, char *argv[]){
     } while (option != 0);
 }
 
-int menu(){
+int menu() {
     int op = 0;
     printf("\n---------------------------------------------------------\n---------------------------------------------------------\n");
     printf("Seleccione el metodo a utilizar: \n1- Biseccion \n2- Regula-falsi \n3- Iteracion de punto fijo \n4- Newton-Raphson \n5- Secante \n0- Finalizar programa\n");
@@ -61,7 +68,7 @@ int menu(){
     return op;
 }
 
-void biseccion(){
+void biseccion() {
     double a, b, cn, cv, tolerancia, e;
     int iteracion = 0;
     printf("Recuerde haber definido previamente la funcion al inicio del codigo\nIngrese el limite inferior del intervalo\n");
@@ -71,40 +78,37 @@ void biseccion(){
     printf("Ingrese la cantidad de cifras decimales de error\n");
     scanf("%lf", &tolerancia);
     tolerancia = pow(10, -tolerancia);
-    if (funcion(a) * funcion(b) < 0){
-        do{
-            if (iteracion == 0){
+    if (funcion(a) * funcion(b) < 0) {
+        do {
+            if (iteracion == 0) {
                 cn = (a + b) / 2;
                 cv = a;
                 e = fabs(cn - cv);
-            }
-            else{
-                if (funcion(a) * funcion(cn) < 0){
+            } else {
+                if (funcion(a) * funcion(cn) < 0) {
                     b = cn;
                     cv = cn;
                     cn = (a + b) / 2;
                     e = fabs(cn - cv);
-                }
-                else if (funcion(b) * funcion(cn) < 0){
+                } else if (funcion(b) * funcion(cn) < 0) {
                     a = cn;
                     cv = cn;
                     cn = (a + b) / 2;
                     e = fabs(cn - cv);
-                }
-                else{
+                } else {
                     break;
                 }
             }
             iteracion++;
         } while (e > tolerancia);
     }
-    printf("\nResultados:\n");
+    printf("\nResultados Biseccion:\n");
     printf("El valor de la raiz es %lf con un error de %lf\n", cn, e);
-    printf("La funcion en dicho punto vale %lf\n",funcion(cn));
+    printf("La funcion en dicho punto vale %lf\n", funcion(cn));
     printf("El problema fue resuelto en %d iteraciones\n", iteracion);
 }
 
-void regulaFalsi(){
+void regulaFalsi() {
     double a, b, cn, cv, tolerancia, e;
     int iteracion = 0;
     printf("Recuerde haber definido previamente la funcion al inicio del codigo\nIngrese el limite inferior del intervalo\n");
@@ -114,50 +118,47 @@ void regulaFalsi(){
     printf("Ingrese la cantidad de cifras decimales de error\n");
     scanf("%lf", &tolerancia);
     tolerancia = pow(10, -tolerancia);
-    if (funcion(a) * funcion(b) < 0){
-        do{
-            if (iteracion == 0){
+    if (funcion(a) * funcion(b) < 0) {
+        do {
+            if (iteracion == 0) {
                 cn = (funcion(b) * a - funcion(a) * b) / (funcion(b) - funcion(a));
                 cv = a;
                 e = fabs(cn - cv);
-            }
-            else{
-                if (funcion(a) * funcion(cn) < 0){
+            } else {
+                if (funcion(a) * funcion(cn) < 0) {
                     b = cn;
                     cv = cn;
                     cn = (funcion(b) * a - funcion(a) * b) / (funcion(b) - funcion(a));
                     e = fabs(cn - cv);
-                }
-                else if (funcion(b) * funcion(cn) < 0){
+                } else if (funcion(b) * funcion(cn) < 0) {
                     a = cn;
                     cv = cn;
                     cn = (funcion(b) * a - funcion(a) * b) / (funcion(b) - funcion(a));
                     e = fabs(cn - cv);
-                }
-                else{
+                } else {
                     break;
                 }
             }
             iteracion++;
         } while (e > tolerancia);
     }
-    printf("\nResultados:\n");
+    printf("\nResultados Regula Falsi:\n");
     printf("El valor de la raiz es %lf con un error de %lf\n", cn, e);
-    printf("La funcion en dicho punto vale %lf\n",funcion(cn));
+    printf("La funcion en dicho punto vale %lf\n", funcion(cn));
     printf("El problema fue resuelto en %d iteraciones\n", iteracion);
 }
 
-void puntoFijo(){
+void puntoFijo() {
     double cn, cv, tolerancia, e;
     int iteracion = 0;
-    printf("Recuerde haber definido previamente la funcion y la funcion auxiliar al inicio del codigo\nIngrese el valor de partida (X0)\n");
+    printf("Recuerde haber definido previamente la funcion y su g(x) (funcionAux) al inicio del codigo\nIngrese el valor de partida (X0)\n");
     scanf("%lf", &cv);
     printf("Ingrese la cantidad de cifras decimales de error\n");
     scanf("%lf", &tolerancia);
     tolerancia = pow(10, -tolerancia);
-    do{
+    do {
         double lim = (funcionAux(cv + (0.01)) - funcionAux(cv)) / (0.01);
-        if (fabs(lim) >= 1){
+        if (fabs(lim) >= 1) {
             printf("El metodo diverge (|g'(x)|>1)");
             break;
         }
@@ -168,13 +169,13 @@ void puntoFijo(){
 
     } while (e > tolerancia);
 
-    printf("\nResultados:\n");
+    printf("\nResultados Punto Fijo:\n");
     printf("El valor de la raiz es %lf con un error de %lf\n", cn, e);
-    printf("La funcion en dicho punto vale %lf\n",funcion(cn));
+    printf("La funcion en dicho punto vale %lf\n", funcion(cn));
     printf("El problema fue resuelto en %d iteraciones\n", iteracion);
 }
 
-void newton_raphson(){
+void newton_raphson() {
     double cn, cv, tolerancia, e;
     int iteracion = 0;
     printf("Recuerde haber definido previamente la funcion y su derivada al inicio del codigo\nIngrese el valor de partida (X0)\n");
@@ -182,8 +183,10 @@ void newton_raphson(){
     printf("Ingrese la cantidad de cifras decimales de error\n");
     scanf("%lf", &tolerancia);
     tolerancia = pow(10, -tolerancia);
-    do{
-        if (fabs(funcionAux(cv)) < (0.001)){
+    do {
+        //double lim = (funcion(cv+0.01)- funcion(cv))/0.01;
+        //if(fabs(lim) < (0.001)){
+        if (fabs(funcionAux(cv)) < (0.00001)) {
             printf("El metodo diverge (|f'(x)| cercano a 0)");
             break;
         }
@@ -192,15 +195,15 @@ void newton_raphson(){
         cv = cn;
         iteracion++;
 
-    } while (e > tolerancia || iteracion >= 5000);
+    } while (e > tolerancia && iteracion < 5000);
 
-    printf("\nResultados:\n");
+    printf("\nResultados Newton-Raphson:\n");
     printf("El valor de la raiz es %lf con un error de %lf\n", cn, e);
-    printf("La funcion en dicho punto vale %lf\n",funcion(cn));
+    printf("La funcion en dicho punto vale %lf\n", funcion(cn));
     printf("El problema fue resuelto en %d iteraciones\n", iteracion);
 }
 
-void secante(){
+void secante() {
     double cn, cv, cvv, tolerancia, e;
     int iteracion = 0;
     printf("Recuerde haber definido previamente la funcion y su derivada al inicio del codigo\nIngrese el valor de partida (cv)\n");
@@ -210,7 +213,7 @@ void secante(){
     printf("Ingrese la cantidad de cifras decimales de error\n");
     scanf("%lf", &tolerancia);
     tolerancia = pow(10, -tolerancia);
-    do{
+    do {
         double aprox = (funcion(cvv) - funcion(cv)) / (cvv - cv);
         cn = cv - (funcion(cv) / aprox);
         e = fabs(cn - cv);
@@ -220,9 +223,9 @@ void secante(){
 
     } while (e > tolerancia || iteracion >= 5000);
 
-    printf("\nResultados:\n");
+    printf("\nResultados Secante:\n");
     printf("El valor de la raiz es %lf con un error de %lf\n", cn, e);
-    printf("La funcion en dicho punto vale %lf\n",funcion(cn));
+    printf("La funcion en dicho punto vale %lf\n", funcion(cn));
     printf("El problema fue resuelto en %d iteraciones\n", iteracion);
 }
 
